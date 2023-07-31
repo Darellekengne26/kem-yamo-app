980dbe941fd9f9baa68f18be62309d76432d19d3
Darellekengne26 <darellekegne@gmail.com>
2023-07-31 16:15:30 +0100

    first commit

---
+++ app/Http/Controllers/UrlsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UrlsController extends Controller
+{
+    public function create()
+    {
+        return view('vendor/welcome');
+    }
+}
